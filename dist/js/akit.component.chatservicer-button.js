b97a1b031843ecfa4add992f872540d61bb41c36
Sander Houttekier <sander.houttekier@gmail.com>
2017-09-14 13:11:46 +0200

    build new version

--- dist/js/akit.component.chatservicer-button.js
+++ dist/js/akit.component.chatservicer-button.js
@@ -143,6 +143,48 @@
 
     ng
         .module('akit.component.chatservicerButton')
+        .service('akit.component.chatservicerButto.chatproxyService', [
+            '$http',
+            '$interval',
+            '$q',
+            'akit.component.chatservicerButton.chatproxyConfig',
+            function ($http, $interval, $q, chatproxyConfig) {
+
+                var API = {};
+
+                function getAvailability(entitykey) {
+                    return $http.get(chatproxyConfig.chatproxyServiceUrl + "/availability?entitykey=" + entitykey, {
+                        overrideErrorHandling: true
+                    })
+                    .then(function (response) {
+                        return response.data;
+                    });
+                }
+
+                function getChatURL(entitykey) {
+                    return $http.get(chatproxyConfig.chatproxyServiceUrl + '/chaturl?entitykey=' + entitykey, {
+                        overrideErrorHandling: true
+                    })
+                    .then(function (response) {
+                        return response.data;
+                    });
+                }
+
+                API.getAvailability = getAvailability;
+                API.getChatURL = getChatURL;
+
+                return API;
+
+            }
+        ]);
+
+})(window.angular);
+
+(function (ng) {
+    'use strict';
+
+    ng
+        .module('akit.component.chatservicerButton')
         .directive('chatservicerButton', [
             '$timeout',
             '$window',
@@ -198,48 +240,6 @@
                         initialize();
                     }
                 };
-
-            }
-        ]);
-
-})(window.angular);
-
-(function (ng) {
-    'use strict';
-
-    ng
-        .module('akit.component.chatservicerButton')
-        .service('akit.component.chatservicerButto.chatproxyService', [
-            '$http',
-            '$interval',
-            '$q',
-            'akit.component.chatservicerButton.chatproxyConfig',
-            function ($http, $interval, $q, chatproxyConfig) {
-
-                var API = {};
-
-                function getAvailability(entitykey) {
-                    return $http.get(chatproxyConfig.chatproxyServiceUrl + "/availability?entitykey=" + entitykey, {
-                        overrideErrorHandling: true
-                    })
-                    .then(function (response) {
-                        return response.data;
-                    });
-                }
-
-                function getChatURL(entitykey) {
-                    return $http.get(chatproxyConfig.chatproxyServiceUrl + 'chaturl?entitykey=' + entitykey, {
-                        overrideErrorHandling: true
-                    })
-                    .then(function (response) {
-                        return response.data;
-                    });
-                }
-
-                API.getAvailability = getAvailability;
-                API.getChatURL = getChatURL;
-
-                return API;
 
             }
         ]);
