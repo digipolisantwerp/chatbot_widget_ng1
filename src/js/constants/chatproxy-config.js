6569270f38b72cff4a407d90a4a801b0e8578037
Sander Houttekier <sander.houttekier@gmail.com>
2017-09-14 11:46:13 +0200

    fix service url

--- src/js/constants/chatproxy-config.js
+++ src/js/constants/chatproxy-config.js
@@ -4,7 +4,7 @@
     ng
         .module('akit.component.chatservicerButton')
         .constant('akit.component.chatservicerButton.chatproxyConfig', {
-            chatproxyServiceUrl: '/srv/chatservicer/d/'
+            chatproxyServiceUrl: '/srv/chatservicer/d'
         });
 
 })(window.angular);
