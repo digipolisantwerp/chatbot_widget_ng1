8b651a03cbdc941b7b61c17a9cad6df76a640a87
Joeri Sebrechts <joeri.sebrechts@digipolis.be>
2018-10-30 10:13:57 +0100

    fix example app

--- example/js/example.js
+++ example/js/example.js
@@ -6,6 +6,9 @@
         .controller('chatbotWidgetCtrl', [
             '$scope',
             function ($scope) {
+                // start new random sessions every time
+                $scope.session1 = Math.random().toString(36).substring(7);
+                $scope.session2 = Math.random().toString(36).substring(7);
             }
         ]);
 
